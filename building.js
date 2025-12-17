d1dc84c3cf24ae476bd23f5bac8878d0cb4e38d0
maedeh <rezaei.maedeh2323@gmail.com>
2025-12-17 15:49:19 +0330

    change style PROJECT SWIPER

--- building.js
+++ building.js
@@ -16,3 +16,39 @@ card.style.setProperty('--x',`${e.clientX-r.left}px`)
 card.style.setProperty('--y',`${e.clientY-r.top}px`)
 })
 })
+
+
+
+
+const projectSwiper = new Swiper('.project-swiper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
